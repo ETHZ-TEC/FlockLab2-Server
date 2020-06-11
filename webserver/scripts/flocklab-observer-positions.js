50d4a58f5d15e06fa49e5474be9152ec24272ce5
rdaforno <reto.daforno@tik.ee.ethz.ch>
2020-06-11 15:43:49 +0200

    observer 3 position added

--- webserver/scripts/flocklab-observer-positions.js
+++ webserver/scripts/flocklab-observer-positions.js
@@ -8,5 +8,6 @@ sensornodes=[
 { node_id:  6, x:268, y:400 },
 { node_id:  5, x:155, y:305 },
 { node_id: 12, x:195, y:190 },
-{ node_id: 10, x:560, y:130 }
+{ node_id: 10, x:560, y:130 },
+{ node_id:  3, x:120, y:400 }
 ];
