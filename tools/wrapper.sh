4cb068359ebd15d2087a8f00d563e859cb5b420c
Reto Da Forno <reto.daforno@tik.ee.ethz.ch>
2020-07-08 19:44:56 +0200

    wrapper updated

--- tools/wrapper.sh
+++ tools/wrapper.sh
@@ -2,19 +2,10 @@
 #
 # a simple wrapper to activate the conda virtual environment before executing a script
 #
-# for conda:
+# for conda, add the following to .bashrc:
 #export PATH=/home/flocklab/conda_venv/bin/python:$PATH
 #[[ -f /scratch/flocklab/conda/bin/conda ]] && eval "$(/scratch/flocklab/conda/bin/conda shell.bash hook)"
 #conda activate py36
-#
-# alternatively, add the above to .bashrc:
-#source ~/.bashrc
 
-which conda > /dev/null 2>&1
-if [ $? -ne 0 ]
-then
-  source ~/.bashrc
-else
-  source ~/pyvenv/bin/activate
-fi
+source ~/.bashrc
 $*
